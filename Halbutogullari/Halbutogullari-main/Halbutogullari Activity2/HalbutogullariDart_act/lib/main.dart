af363b05cea9fdd161a5612310c3fc210be193ea
MehmetAliHalbutii <80024029+MehmetAliHalbutii@users.noreply.github.com>
2021-03-04 13:38:46 +0800

    Update main.dart

--- Halbutogullari/Halbutogullari-main/Halbutogullari Activity2/HalbutogullariDart_act/lib/main.dart
+++ Halbutogullari/Halbutogullari-main/Halbutogullari Activity2/HalbutogullariDart_act/lib/main.dart
@@ -18,25 +18,25 @@ void main() {
       var sum = g / a1;
 
       print('$sum Liter Of Leaded Gasoline');
-      print('Thank you');
+      print('Thank you!');
     } else if (e == 'unleaded') {
       print('Input Amount: ');
       var g = int.parse(stdin.readLineSync());
       var sum = g * a2;
       print('$sum Liter Of Unleaded Gasoline');
-      print('Thank you');
+      print('Thank you!');
     } else if (e == 'diesel') {
       print('Input Amount: ');
       var g = int.parse(stdin.readLineSync());
       var sum = g / a3;
       print('$sum Liter Of Diesel Gasoline');
-      print('Thank you');
+      print('Thank you!');
     } else if (e == 'biodiesel') {
       print('Input Amount: ');
       var g = int.parse(stdin.readLineSync());
       var sum = g / a4;
       print('$sum Liter Of BioDiesel Gasoline');
-      print('Thank you');
+      print('Thank you!');
     } else {
       print('No Transaction');
     }
@@ -49,25 +49,25 @@ void main() {
         var g = int.parse(stdin.readLineSync());
         var sum = g * a1;
         print('Total Cash Amount:$sum');
-        print('Thank you');
+        print('Thank you!');
       } else if (e == 'unleaded') {
         print('Input How many Liters: ');
         var g = int.parse(stdin.readLineSync());
         var sum = g * a2;
         print('Total Cash Amount:$sum');
-        print('Thank you');
+        print('Thank you!');
       } else if (e == 'diesel') {
         print('Input How many Liters: ');
         var g = int.parse(stdin.readLineSync());
         var sum = g * a3;
         print('Total Cash Amount:$sum');
-        print('Thank you');
+        print('Thank you!');
       } else if (e == 'biodiesel') {
         print('Input How many Liters ');
         var g = int.parse(stdin.readLineSync());
         var sum = g * a4;
         print('Total Cash Amount:$sum');
-        print('Thank you');
+        print('Thank you!');
       } else {
         print('No Transaction');
       }
